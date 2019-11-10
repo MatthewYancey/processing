5ee8e9aa3021bbb3798093f3a9a7626178edd37b
Lori <lori@loris-imac.attlocal.net>
2019-11-10 08:30:46 -0500

    added sliders

--- wall_art17/dash.js
+++ wall_art17/dash.js
@@ -1,30 +1,34 @@
 function Dash(person) {
-    var default_age = 30;
-    var max_year = 2011;
-    var min_year = 1680; //true min is 1615
+    // y and boarder
+    this.y1_jitter = random(3);
 
-    // boarder
-    var boarder = 5
-    var y1_jitter = random(3);
+    this.setup = function(){
 
-    // x and y
-    var x = map(random(100), 0, 100, boarder, width - boarder);
-    var y1 = map(person[0], min_year, max_year, boarder - y1_jitter, height - boarder, true);
-    if (person[1] == -1){
-        var y2 = map(person[0] + default_age, min_year, max_year, boarder, height - boarder, true);
-    } else {
-        var y2 = map(person[1], min_year, max_year, boarder, height - boarder, true);
+        // x and y
+        this.x = map(random(100), 0, 100, boarder, width - boarder);
+        this.y1 = map(person[0], min_year, max_year, boarder - this.y1_jitter, height - boarder, true);
+        if (person[1] == -1){
+            this.y2 = map(person[0] + default_age, min_year, max_year, boarder, height - boarder, true);
+        } else {
+            this.y2 = map(person[1], min_year, max_year, boarder, height - boarder, true);
+        }
+
+        // color
+        this.r1 = random(122);
+        this.blue = random(50);
+        this.alpha = map(random(100), 0, 100, 100, 225);
+
+        // stroke
+        noStroke();
+        stroke(this.r1, this.r1, this.r1 + this.blue, this.alpha);
+        line(this.x, this.y1, this.x, this.y2); 
     }
 
-    // color
-    var r1 = random(122);
-    var r2 = random(122);
-    var r3 = random(122);
-    var blue = random(50);
-    var alpha = map(random(100), 0, 100, 100, 225);
+    this.show = function() {
+        noStroke();
+        stroke(this.r1, this.r1, this.r1 + this.blue, this.alpha);
+        line(this.x, this.y1, this.x, this.y2);     
+    }
 
-    // stroke
-    noStroke();
-    stroke(r1, r1, r1 + blue, alpha);
-    line(x, y1, x, y2); 
+    this.setup();
 }
--- wall_art17/sketch.js
+++ wall_art17/sketch.js
@@ -1,22 +1,55 @@
-var size = [1.5, 3];
-var size_x = 200;
-var inc = 0.005;
-var noise_range = [160, 240];
+var size = [1, 2.6];
+var size_x = 320;
+var dashes = []
+var boarder = 5;
+var default_age = 30;
+var max_year = 2011;
+var min_year = 1680; //true min is 1615
 
-function setup() {
+let slider_lower;
+let slider_upper;
+let slider_inc;
+
+
+function setup() {  
   createCanvas(size[0] * size_x, size[1] * size_x);
   background(255, 255, 255);
+  
+  slider_lower = createSlider(0, 255, 100);
+  slider_lower.position(width + 20, 10);
+  text('red', rSlider.x * 2 + rSlider.width, 35);
+
+  slider_upper = createSlider(0, 255, 200);
+  slider_upper.position(width + 20, 30);
+  text('green', gSlider.x * 2 + gSlider.width, 65);
+
+  slider_inc = createSlider(0, 100, 50);
+  slider_inc.position(width + 20, 50);
+  text('blue', bSlider.x * 2 + bSlider.width, 95);
+
+
+  // lines
+  for (var i = 0; i < dates.length; i++){
+    dashes[i] = new Dash(dates[i]);
+  }
+
 }
 
 function draw() {
   // background
+  let lower_bound = slider_lower.value();
+  let upper_bound = slider_upper.value();
+  let inc = map(slider_inc.value(), 0, 100, 0.0005, 0.01);
   var yoff = 0;
+
   loadPixels();
   for (var y = 0; y < height; y++){
     var xoff = 0;
     for (var x = 0; x < width; x++){      
       var index = (x + y * width) * 4;
-      var r = map(noise(xoff, yoff), 0, 1, noise_range[0], noise_range[1]);
+
+
+      var r = map(noise(xoff, yoff), 0, 1, lower_bound, upper_bound);
       pixels[index + 0] = r;
       pixels[index + 1] = r;
       pixels[index + 2] = r;
@@ -27,10 +60,8 @@ function draw() {
   }
   updatePixels();
 
-
-  // lines
-  for (var i = 0; i < dates.length; i++){
-    new Dash(dates[i])
+  for (var i = 0; i < dashes.length; i++){
+    dashes[i].show();
   }
-  noLoop();
+
 }
